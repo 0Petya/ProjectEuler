51a68d0d40ebac800c82ed350cc68754365a062f
Peter Tran <Peter1992t@gmail.com>
2015-12-14 22:01:05 -0600

    Solved problem 4.

--- Haskell/4.hs
+++ Haskell/4.hs
@@ -4,8 +4,17 @@
   Find the largest palindrome made from the product of two 3-digit numbers.
 -}
 
-isPalindrome :: (Eq a) => [a] -> Bool
-isPalindrome xs = xs == reverse xs
+digs :: Integral x => x -> [x]
+digs 0 = []
+digs x = digs (div x 10) ++ [mod x 10]
+
+isPalindrome :: Integer -> Bool
+isPalindrome xs = dxs == reverse dxs
+  where dxs = digs xs
+
+threeDigitProductPermutations :: [Integer]
+threeDigitProductPermutations = [x * y| x <- threeDigits, y <- threeDigits]
+  where threeDigits = [100..999]
 
 main :: IO ()
-main = error "not implemented"
+main = print . maximum . filter isPalindrome $ threeDigitProductPermutations
