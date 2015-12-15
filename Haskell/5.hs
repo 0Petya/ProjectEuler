4460843702c254934fd729da31d2686254c42c8b
Peter Tran <peter.tran@rainhail.com>
2015-12-15 16:08:37 -0600

    Reworked problem 5 to be significantly more simple and quick. Next time I should read the problem more clearly and think about it a little more to realize I'm just trying to find the least common multiple of [1..20]. 'What is the smallest positive number that is evenly divisible...', ok, Project Euler...

--- Haskell/5.hs
+++ Haskell/5.hs
@@ -4,14 +4,5 @@
   What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 -}
 
-import Data.List (groupBy, elemIndex)
-import Data.Maybe (fromJust)
-
-modulateTuples :: [[(Integer, Integer)]] -> [[Integer]]
-modulateTuples xs = map (\x -> map (\y -> mod (fst y) (snd y)) x) $ xs
-
-tupleMultiples :: [Integer] -> [[(Integer, Integer)]]
-tupleMultiples xs = groupBy (\x y -> fst x == fst y) [(y, x) | y <- [2,4..], x <- xs]
-
 main :: IO ()
-main = print ((fromJust . elemIndex [] . map (filter (/= 0)) . modulateTuples . tupleMultiples $ [1..20]) * 2 + 2)
+main = print . foldl1 lcm $ [1..20]
