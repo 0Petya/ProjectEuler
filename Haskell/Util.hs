4221b5fff28c8c0d7478660c3dcb78dff5e80d17
Peter Tran <peter.tran@rainhail.com>
2015-12-16 09:33:37 -0600

    Solved problem 9.

--- Haskell/Util.hs
+++ Haskell/Util.hs
@@ -1,8 +1,4 @@
-module Util (isPrime, primes, fibonaccis, isPalindrome, toDigits) where
-
-isPrime :: Integer -> Bool
-isPrime 1 = False
-isPrime n = null [x | x <- [2..floor . sqrt $ (fromInteger n :: Float)], mod n x == 0]
+module Util (primes, fibonaccis, pythagTriplets, isPrime, isPalindrome, toDigits) where
 
 primes :: [Integer]
 primes = filter isPrime [2..]
@@ -10,6 +6,13 @@ primes = filter isPrime [2..]
 fibonaccis :: [Integer]
 fibonaccis = go 1 2
   where go x y = x:go y (x + y)
+
+pythagTriplets :: [(Integer, Integer, Integer)]
+pythagTriplets = [(a,b,c) | c <- [1..], b <- [1..c], a <- [1..b], (a ^ 2) + (b ^ 2) == (c ^ 2)]
+
+isPrime :: Integer -> Bool
+isPrime 1 = False
+isPrime n = null [x | x <- [2..floor . sqrt $ (fromInteger n :: Float)], mod n x == 0]
 
 isPalindrome :: (Eq a) => [a] -> Bool
 isPalindrome x = x == reverse x
