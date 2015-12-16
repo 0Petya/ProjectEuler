9c02e5521d70105faceb27d5b622c53f5cf1b313
Peter Tran <Peter1992t@gmail.com>
2015-12-15 20:22:56 -0600

    Solved problem 8.

--- Haskell/Util.hs
+++ Haskell/Util.hs
@@ -1,4 +1,4 @@
-module Util (isPrime, primes, fibonaccis, isPalindrome) where
+module Util (isPrime, primes, fibonaccis, isPalindrome, toDigits) where
 
 isPrime :: Integer -> Bool
 isPrime 1 = False
@@ -13,3 +13,7 @@ fibonaccis = go 1 2
 
 isPalindrome :: (Eq a) => [a] -> Bool
 isPalindrome x = x == reverse x
+
+toDigits :: Integral x => x -> [x]
+toDigits 0 = []
+toDigits x = toDigits (div x 10) ++ [mod x 10]
